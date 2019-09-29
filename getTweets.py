5438a96b546f186f72a6c1dd95b0c3de5731e8e1
atdan <atumadann@gmail.com>
2019-09-29 16:40:53 +0100

    Added redis

---
+++ getTweets.py
@@ -0,0 +1,53 @@
+from tweet_store import TweetStore
+
+from config.settings import *
+import tweepy
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+# print('CONSUMER SECRET', CONSUMER_SECRET)
+
+api = tweepy.API(auth)
+store = TweetStore()
+
+
+class StreamListener(tweepy.StreamListener):
+
+    def on_status(self, status):
+        tweet_obj = {
+            'id_str': status.id_str,
+            'source': status.source,
+            'text': status.text,
+            'retweeted': status.retweeted,
+            'retweet_count': status.retweet_count,
+            'created_at': str(status.created_at),
+            'username': status.user.name,
+            'profile_img_url': status.user.profile_image_url,
+            'followers': status.user.followers_count,
+            'user_id': status.user.id,
+        }
+
+        # print(status)
+        # print(tweet_obj)
+        store.push(tweet_obj)
+        # print('Pushed to redis: ', tweet_obj)
+        print("\n\n\n\n")
+
+
+    def on_error(self, status_code):
+        if status_code == 420:
+            print('Error 420')
+            # returning False in on_data disconnects the stream
+            return False
+
+
+# user = api.me()
+
+# print('name: ', user.name)
+
+# print("TRACK TERMS 1: ", TRACK_TERMS)
+stream_listener = StreamListener()
+
+stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
+
+stream.filter(track=TRACK_TERMS)
---
+++ tweet_store.py
@@ -0,0 +1,56 @@
+import json
+
+from kafka import KafkaProducer
+import redis
+
+class TweetStore:
+    # Redis Configuration
+    redis_host = "localhost"
+    redis_port = 6379
+    redis_password = ""
+
+    # Tweet Configuration
+    redis_key = 'tweets'
+    num_tweets = 20
+    trim_threshold = 100
+
+    # Kafka config
+    KAFKA_HOST_URL = '192.168.0.22:9092,192.168.0.22:9093,192.168.0.22:9094'
+    producer = KafkaProducer(bootstrap_servers=KAFKA_HOST_URL,
+                             value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+    topic_name = "atuma_twitter_stream_topic"
+
+    def __init__(self):
+        self.db = r = redis.Redis(
+            host=self.redis_host,
+            port=self.redis_port
+        )
+        self.trim_count = 0
+
+    def redis_to_kafka(self, data, limit=15):
+        # KAFKA_HOST_URL = '52.19.199.252:9092,52.19.199.252:9093,52.19.199.252:9094'
+        tweets = []
+        print("kafka sending.....")
+        print('broker', self.KAFKA_HOST_URL)
+        print('producer', self.producer)
+        ack = self.producer.send(self.topic_name, data)
+        print("Ack: ", ack)
+        print("\n\n\n")
+
+        for item in self.db.lrange(self.redis_key, -1 ):
+            tweet_obj = json.loads(item)
+            tweets.append(tweet_obj)
+
+        return tweets
+
+    def push(self, data):
+        self.db.lpush(self.redis_key, json.dumps(data))
+        self.trim_count += 1
+
+        for item in self.db.lrange(self.redis_key, -1):
+            tweet_obj = json.loads(item)
+            self.redis_to_kafka(tweet_obj)
+        # Periodically trim the list so it doesn't grow too large.
+        # if self.trim_count > 100:
+        #     self.db.ltrim(self.redis_key, 0, self.num_tweets)
+        #     self.trim_count = 0
